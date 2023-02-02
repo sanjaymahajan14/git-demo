2418aabc5ea77ff6b96128fbd1640d04118100c8
Sanjay Mahajan <sanjay.mahajan@esds.co.in>
2023-02-02 18:32:40 +0530

    added if condition to check the data

--- src/main/java/com/parth/App.java
+++ src/main/java/com/parth/App.java
@@ -9,6 +9,9 @@ public class App
 	
 	public void addData(String data) {
 		System.out.println("data ="+data);
+		if(data.equals("Atharva Softech")) {
+			System.out.println("Welcome!");
+		}
 	}
     public static void main( String[] args )
     {
